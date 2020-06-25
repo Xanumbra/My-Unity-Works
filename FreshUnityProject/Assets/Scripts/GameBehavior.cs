369306d5ceaab40e8a5a3ef86e38369f0c7f7acd
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-25 19:07:06 +0300

    Update GameBehavior.cs
    Now the game restarts when the "You Won!" Button is pressed.

--- FreshUnityProject/Assets/Scripts/GameBehavior.cs
+++ FreshUnityProject/Assets/Scripts/GameBehavior.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameBehavior : MonoBehaviour
 {
     public string labelText = "Collect all 4 items and win your freedom!";
@@ -21,6 +21,8 @@ public class GameBehavior : MonoBehaviour
             {
                 labelText = "You've found all the items!";
                 showWinScreen = true;
+
+                Time.timeScale = 0f;
             }
             else
             {
@@ -50,7 +52,8 @@ public class GameBehavior : MonoBehaviour
         {
             if(GUI.Button(new Rect(Screen.width/2 - 100,Screen.height/2 - 50,200,100),"YOU WON!"))
             {
-
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
             }
         }
     }
