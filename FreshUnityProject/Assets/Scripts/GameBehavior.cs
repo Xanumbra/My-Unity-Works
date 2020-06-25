9f0a565a369ebf062b6dadc6efc24ab364949b1c
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-25 18:49:40 +0300

    Update GameBehavior.cs
    Added OnGUI Method to show player lives, items collected and the main objective.

--- FreshUnityProject/Assets/Scripts/GameBehavior.cs
+++ FreshUnityProject/Assets/Scripts/GameBehavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public int maxItems = 4;
     private int _itemsCollected = 0;
     public int Items
     {
@@ -11,18 +13,35 @@ public class GameBehavior : MonoBehaviour
         set {
             _itemsCollected = value;
             Debug.LogFormat("Items: {0}", _itemsCollected);
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
         }
+        
     }
 
-    private int _playerHP = 10;
+    private int _playerLives = 3;
 
     public int HP
     {
-        get { return _playerHP; }
+        get { return _playerLives; }
         set {
-            _playerHP = value;
-            Debug.LogFormat("Lives: {0}", _playerHP);
+            _playerLives = value;
+            Debug.LogFormat("Lives: {0}", _playerLives);
             }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20,20,150,25),"Player Health:"+_playerLives);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
     }
 
 }
