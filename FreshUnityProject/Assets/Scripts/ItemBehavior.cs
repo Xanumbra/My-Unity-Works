05b2ce84a2fbf28cc37af4140bcbdb850f76dd4a
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-25 18:37:51 +0300

    Update ItemBehavior.cs
    Added gameManager to ItemBehavior so that it can update Items that are picked up.

--- FreshUnityProject/Assets/Scripts/ItemBehavior.cs
+++ FreshUnityProject/Assets/Scripts/ItemBehavior.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+    }
+
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
@@ -11,6 +20,7 @@ public class ItemBehavior : MonoBehaviour
             Destroy(this.transform.parent.gameObject);
 
             Debug.Log("Item collected!");
+            gameManager.Items += 1;
         }
     }
 }
