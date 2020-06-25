18914ad76881c8597fab3c1b716183bc1db5881c
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-26 00:46:18 +0300

    Update Utilities.cs
    Added UpdateDeathCount so that deathcount is updated and saved.

--- FreshUnityProject/Assets/Scripts/Utilities.cs
+++ FreshUnityProject/Assets/Scripts/Utilities.cs
@@ -7,6 +7,12 @@ public static class Utilities
 {
     public static int playerDeaths = 0;
 
+    public static string UpdateDeathCount(ref int countReference)
+    {
+        countReference += 1;
+        return "Next time you'll be at number " + countReference;
+    }
+
     public static void RestartLevel()
     {
         SceneManager.LoadScene(0);
@@ -17,6 +23,9 @@ public static class Utilities
         SceneManager.LoadScene(sceneIndex);
         Time.timeScale = 1.0f;
 
+        Debug.Log("Player deaths: " + playerDeaths);
+        string message = UpdateDeathCount(ref playerDeaths);
+        Debug.Log("Player deaths: " + playerDeaths);
         return true;
     }
 }
