920f6615422734055c606e8b5416a1d7875eba1e
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-25 17:47:45 +0300

    Created EnemyBehavior Script
    Simple Enemy soon to have these:
    ->Patrolling around the level
    ->Attacking the player

---
+++ FreshUnityProject/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
+}
