3d1be7f5f03f34d84651352d9d2e9f02e3bdf05a
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-25 18:55:17 +0300

    Update GameBehavior.cs
    Shows win screen when collected maxItems in the level.

--- FreshUnityProject/Assets/Scripts/GameBehavior.cs
+++ FreshUnityProject/Assets/Scripts/GameBehavior.cs
@@ -6,6 +6,9 @@ public class GameBehavior : MonoBehaviour
 {
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
+
+    public bool showWinScreen = false;
+
     private int _itemsCollected = 0;
     public int Items
     {
@@ -17,6 +20,7 @@ public class GameBehavior : MonoBehaviour
             if (_itemsCollected >= maxItems)
             {
                 labelText = "You've found all the items!";
+                showWinScreen = true;
             }
             else
             {
@@ -41,5 +45,13 @@ public class GameBehavior : MonoBehaviour
         GUI.Box(new Rect(20,20,150,25),"Player Health:"+_playerLives);
         GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
         GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if (showWinScreen)
+        {
+            if(GUI.Button(new Rect(Screen.width/2 - 100,Screen.height/2 - 50,200,100),"YOU WON!"))
+            {
+
+            }
+        }
     }
 }
