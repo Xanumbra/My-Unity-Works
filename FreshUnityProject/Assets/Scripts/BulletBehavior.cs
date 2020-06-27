f642678c513289b346fe31f403e67f184a0bdae5
Ertuğ Obalar <eobalar@hotmail.com>
2020-06-27 20:12:54 +0300

    Update BulletBehavior.cs
    Added ignore collisions for player and destroy on collisions with environment

--- FreshUnityProject/Assets/Scripts/BulletBehavior.cs
+++ FreshUnityProject/Assets/Scripts/BulletBehavior.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class BulletBehavior : MonoBehaviour
 {
     public float onscreenDelay = 3f;
+    private Collider _playerCol;
     // Update is called once per frame
+    private void Start()
+    {
+        _playerCol = GameObject.Find("Player").GetComponent<Collider>();
+        Physics.IgnoreCollision(_playerCol, this.gameObject.GetComponent<Collider>());
+    }
     void Update()
     {
         Destroy(this.gameObject, onscreenDelay);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
